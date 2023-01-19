b39060b93e9bdb806996cc5de1ffb4e1ef620d4c
jean-marc strauven <jms@grazulex.be>
2023-01-19 20:54:10 +0100

    add trans serach

--- app/Filament/Resources/SearchResource/Widgets/OfficeOverview.php
+++ app/Filament/Resources/SearchResource/Widgets/OfficeOverview.php
@@ -11,17 +11,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class OfficeOverview extends BaseWidget
 {
-
-   public static function getModelLabel(): string
-   {
-       return __('filament::resources/pages/search.title');
-   }
-
-   public static function getPluralModelLabel(): string
-   {
-       return __('filament::resources/pages/search.title_plural');
-   }
-   
     protected int | string | array $columnSpan = 'full';
 
     public ?Model $record = null;
@@ -35,30 +24,34 @@ class OfficeOverview extends BaseWidget
     {
         return [
             Tables\Columns\TextColumn::make('office.name')
+               ->label(__('filament::resources/widgets/office.table.name.label'))
                ->description(fn (OfficeSearch $record): string => $record->office->description)
                ->searchable()
                ->sortable(),
             Tables\Columns\TextColumn::make('distance')
+               ->label(__('filament::resources/widgets/office.table.distance.label'))
                ->sortable()
                ->suffix('km')
-               ->tooltip('Distance from your home to this office'),
-            Tables\Columns\BooleanColumn::make('office.is_free')
-               ->label('Free')
-               ->tooltip('The person who offers this sharing does it for free '),
-            Tables\Columns\BooleanColumn::make('office.have_desk')
-               ->label('Dedicated desk')
-               ->tooltip('There is a dedicated desk/table for you'),
+               ->tooltip(__('filament::resources/widgets/office.table.distance.tooltip')),
+            Tables\Columns\IconColumn::make('office.is_free')
+               ->label(__('filament::resources/widgets/office.table.is_free.label'))
+               ->boolean(true)
+               ->tooltip(__('filament::resources/widgets/office.table.is_free.tooltip')),
+            Tables\Columns\IconColumn::make('office.have_desk')
+               ->label(__('filament::resources/widgets/office.table.have_desk.label'))
+               ->boolean(true)
+               ->tooltip(__('filament::resources/widgets/office.table.have_desk.tooltip')),
             Tables\Columns\TagsColumn::make('office.days')
-               ->label('Days')
-               ->tooltip('The days you can join us'),
+               ->label(__('filament::resources/widgets/office.table.days.label'))
+               ->tooltip(__('filament::resources/widgets/office.table.days.tooltip')),
             Tables\Columns\BadgeColumn::make('office.tranquility')
-               ->label('Tranquility')
+               ->label(__('filament::resources/widgets/office.table.tranquility.label'))
                ->suffix('/5')
-               ->tooltip('Rating out of 5 about the tranquility of the workspace (presence of children, noisy animals, etc.)'),
+               ->tooltip(__('filament::resources/widgets/office.table.tranquility.tooltip')),
             Tables\Columns\BadgeColumn::make('office.workspace')
-               ->label('Workspace')
+               ->label(__('filament::resources/widgets/office.table.workspace.label'))
                ->suffix('/5')
-               ->tooltip('Rate about proposed workspace size out of 5'),
+               ->tooltip(__('filament::resources/widgets/office.table.workspace.tooltip')),
 
         ];
     }
@@ -67,10 +60,12 @@ class OfficeOverview extends BaseWidget
     {
         return [
             Action::make('detail')
+               ->label(__('filament::resources/widgets/office.table.detail.label'))
                ->color('secondary')
                ->icon('heroicon-s-eye')
                ->url(fn (OfficeSearch $record): string => route('filament.resources.searches.detail', $record->office)),
             Action::make('contact')
+               ->label(__('filament::resources/widgets/office.table.contact.label'))
                ->color('success')
                ->icon('heroicon-s-pencil')
                ->url(fn (OfficeSearch $record): string => route('filament.resources.offices.edit', $record->office)),
---
+++ lang/vendor/filament/fr/resources/widgets/office.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+   'table' => [
+      'name' =>  [
+          'label' => 'Nom',
+      ],
+      'distance' =>  [
+          'label' => 'Distance',
+          'tooltip' => 'Distance en km',
+      ],
+      'is_free' =>  [
+          'label' => 'Gratuit',
+          'tooltip' => 'Le bureau est-il gratuit ?',
+      ],
+      'have_desk' =>  [
+          'label' => 'Espace de travail',
+          'tooltip' => 'Il y a-t-il un espace de travail prévu autre qu\'une table ?',
+      ],
+      'days' =>  [
+          'label' => 'Jours',
+          'tooltip' => 'Quels jours le bureau est-il disponible ?',
+      ],
+      'tranquility' =>  [
+          'label' => 'Tranquillité',
+          'tooltip' => 'Quel est le niveau de tranquillité ?',
+      ],
+      'workspace' =>  [
+          'label' => 'Espace de travail',
+          'tooltip' => 'Quel est le niveau d\'espace de travail ?',
+      ],
+      'detail' =>  [
+          'label' => 'Détails',
+      ],
+      'contact' =>  [
+          'label' => 'Contact',
+      ],
+  ],
+];
